41767b454b17f77686b02dd526442a067dcd4755
Darkkal44 <darkkal44@gmail.com>
2023-04-14 23:05:25 +0530

    Updated the preview

---
+++ Themes/Everforest/.config/spicetify/Extensions/comfy.js
@@ -0,0 +1,9 @@
+(() => {
+  const script = document.createElement("SCRIPT");
+  script.setAttribute("type", "text/javascript");
+  script.setAttribute(
+    "src",
+    "https://comfy-themes.github.io/Spicetify/Comfy/comfy.script.js"
+  );
+  document.head.appendChild(script);
+})();
