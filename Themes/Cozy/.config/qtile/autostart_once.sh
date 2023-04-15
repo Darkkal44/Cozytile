cebd3044ff907d78237e8a16f00bb4872043516e
Darkkal44 <darkkal44@gmail.com>
2023-04-15 10:57:02 +0530

    cleaned dots

---
+++ Themes/Cozy/.config/qtile/autostart_once.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+#!/usr/bin/bash
+wal -b 282738 -i ~/Wallpaper/Aesthetic2.png &&
+sleep 4 &&
+picom --config ~/.config/picom/picom.conf &
+
+## AUTO START APPLICATIONS/sCRIPTS
+# examples:
+#chromium &
+#pactl set-sink-port 0 analog-output-headphones &
+##ADD YOUT STUFF BELOW!
+
---
+++ Themes/Sakura/.config/qtile/autostart_once.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+wal -b 282738 -i ~/Wallpaper/120_-_KnFPX73.jpg &&
+sleep 4 &&
+picom --config ~/.config/picom/picom.conf &
+#chromium &
+#pactl set-sink-port 0 analog-output-headphones &
