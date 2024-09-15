850bd2316d6427997b5531669ae59af42df13d6d
darkkal44 <darkkal44@gmail.com>
2024-09-15 17:19:08 +0530

    wow

--- nvim/lua/custom/README.md
+++
@@ -1,3 +0,0 @@
-# Example_config
-
-This can be used as an example custom config for NvChad, this branch is a minimal one. Do check the feature_full branch if you need all the ease in your config.
--- nvim/lua/custom/init.lua
+++
@@ -1,7 +0,0 @@
--- local autocmd = vim.api.nvim_create_autocmd
-
--- Auto resize panes when resizing nvim window
--- autocmd("VimResized", {
---   pattern = "*",
---   command = "tabdo wincmd =",
--- })
